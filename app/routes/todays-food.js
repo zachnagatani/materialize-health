50164d54b2730988ebaded9c5e85d6f2dadd0019
znagatani <znagatani@gmail.com>
2016-08-01 18:29:25 -0700

    Feat: Remove Item from Database on Remove

--- app/routes/todays-food.js
+++ app/routes/todays-food.js
@@ -6,29 +6,31 @@ export default Ember.Route.extend({
 
 	// Hook the health-data service in as a model to use on my page
 	model() {
-		return this.get('healthData');
+		return this.store.findAll('item');
 	},
 
 	actions: {
-		removeItem(index) {
-			var self = this;
+		removeItem(item) {
+			// var self = this;
 
-			// Store property paths for easy access
-			let todaysPath = this.get('healthData').todaysFood;
-			let caloriesPath = 'healthData.calories';
+			// // Store property paths for easy access
+			// let todaysPath = this.get('healthData').todaysFood;
+			// let caloriesPath = 'healthData.calories';
 
-  			this.set(caloriesPath, this.get(caloriesPath) - Math.round(todaysPath[index].fields.nf_calories));
+  	// 		this.set(caloriesPath, this.get(caloriesPath) - Math.round(todaysPath[index].fields.nf_calories));
 
-  			todaysPath.removeAt(index);
+  	// 		todaysPath.removeAt(index);
 
-  			// Because Ember adds its own meta-object to the array,
-  			// check to see if its length is zero. The length property
-  			// still accurately reflects our data. If length is zero
-  			// set the foodAdded property to false in order to remove
-  			// the table from our display
-  			if (todaysPath.length === 0) {
-  				self.set('healthData.foodAdded', false);
-  			}
+  	// 		// Because Ember adds its own meta-object to the array,
+  	// 		// check to see if its length is zero. The length property
+  	// 		// still accurately reflects our data. If length is zero
+  	// 		// set the foodAdded property to false in order to remove
+  	// 		// the table from our display
+  	// 		if (todaysPath.length === 0) {
+  	// 			self.set('healthData.foodAdded', false);
+  	// 		}
+
+  			item.destroyRecord();
 		},
 
 		hideResults() {
